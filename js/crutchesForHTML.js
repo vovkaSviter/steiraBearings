a35d94be4df878c8c9bb877818ef9954daac04df
vovkaSviter <vovka.sviter@gmail.com>
2017-12-22 05:03:57 +0300

    In process...

--- js/crutchesForHTML.js
+++ js/crutchesForHTML.js
@@ -43,6 +43,37 @@ $(document).ready(function(){
             $(slider).css('height' , width*2);
         });
 
+
+                //  Item page
+
+        $(function(){
+            var block = $('.itemSubNav .block>div');
+            var width = block.width();
+
+            $(block).css('height' , width/1.75);
+        });
+
+        $(function(){
+            var mainPhoto = $('.mainPhoto');
+            var width = $(mainPhoto).width();
+
+            $(mainPhoto).css('height' , width*.75);
+        });
+
+        $(function(){
+            var tag = $('.tags>div');
+            var width = $(tag).width();
+
+            $(tag).css('height' , width);
+        });
+
+        $(function(){
+            var secondaryPhoto = $('.secondaryPhoto>div');
+            var width = $(secondaryPhoto).width();
+
+            $(secondaryPhoto).css('height' , width);
+        });
+
 });
 
 
