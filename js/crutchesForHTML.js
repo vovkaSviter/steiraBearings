5753cedb0a216fc4de25b7200053a53ceee4ab4e
vovkaSviter <vovka.sviter@gmail.com>
2017-12-25 14:01:38 +0300

    Voidddd

--- js/crutchesForHTML.js
+++ js/crutchesForHTML.js
@@ -99,28 +99,88 @@ $(document).ready(function(){
 
         $(function(){
             var switchItem = $('.switch>div');
+            catKeeper = $('.catKeeper>.row');
+
             $(switchItem).click(function() {
                 var catType = $(this).attr('data-catType');
-                var catKeeper = $('.catKeeper');
                 $(catKeeper).attr('data-type' , catType);
 
                 if ($(catKeeper).attr('data-type') == 'list' ){
                     var i;
                     var itemsList = '';
                     for(i = 0; i < 8; ++i){
-                        itemsList += "<p>List paragraph</p>";
+                        itemsList += "<div class=\"catItemList col-xs-12\">\n" +
+                            "                                            <div class=\"row\">\n" +
+                            "                                                <div class=\"imgKeeper col-xs-12 col-md-2\" style=\"height: 50px; background: cadetblue\"></div>\n" +
+                            "                                                <div class=\"title col-xs-12 col-md-4\">\n" +
+                            "                                                    Подшипник 51100\n" +
+                            "                                                </div>\n" +
+                            "                                                <div class=\"hidden-xs hidden-sm col-md-2 inStock\">\n" +
+                            "                                                    <span class=\"stockCheck\">g</span>\n" +
+                            "                                                    В наличии\n" +
+                            "                                                </div>\n" +
+                            "                                                <div class=\"hidden-xs hidden-sm col-md-2 subDimensions\">\n" +
+                            "                                                    <div>\n" +
+                            "                                                        <span>L</span>\n" +
+                            "                                                        <span>W</span>\n" +
+                            "                                                        <span>M</span>\n" +
+                            "                                                    </div>\n" +
+                            "                                                    <div class=\"values\">\n" +
+                            "                                                        <span>33</span>\n" +
+                            "                                                        <span>34</span>\n" +
+                            "                                                        <span>135</span>\n" +
+                            "                                                    </div>\n" +
+                            "                                                </div>\n" +
+                            "\n" +
+                            "                                                <div class=\"col-xs-12 col-md-2 addOrder\">\n" +
+                            "                                                    <div class=\"addToList\">\n" +
+                            "                                                        <a href=\"#\" class=\"smallTxt\">Отложить в список &rarr;</a>\n" +
+                            "                                                    </div>\n" +
+                            "                                                    <div class=\"orderNow\">\n" +
+                            "                                                        <div>Заказать сейчас!</div>\n" +
+                            "                                                    </div>\n" +
+                            "                                                </div>\n" +
+                            "\n" +
+                            "                                            </div>\n" +
+                            "                                        </div>";
                         $(catKeeper).html(itemsList);
                     }
                 } else {
                     var x;
                     var itemsMasonry = '';
                     for(x = 0; x < 12; ++x){
-                        itemsMasonry += "<p>2</p>";
+                        itemsMasonry += "<div class=\"col-xs-12 col-md-3\">\n" +
+                            "                                            <div class=\"familiarCard catTypeMasonry\">\n" +
+                            "                                                <div class=\"imgKeeper\"></div>\n" +
+                            "                                                <h2>Подшипник 51102</h2>\n" +
+                            "                                                <div class=\"subDimensions\">\n" +
+                            "                                                    <div>\n" +
+                            "                                                        <span>L</span>\n" +
+                            "                                                        <span>W</span>\n" +
+                            "                                                        <span>M</span>\n" +
+                            "                                                    </div>\n" +
+                            "                                                    <div class=\"values\">\n" +
+                            "                                                        <span>335</span>\n" +
+                            "                                                        <span>340</span>\n" +
+                            "                                                        <span>035</span>\n" +
+                            "                                                    </div>\n" +
+                            "                                                </div>\n" +
+                            "                                                <div class=\"addToList\">\n" +
+                            "                                                    <a href=\"#\" class=\"smallTxt\">Отложить в список &rarr;</a>\n" +
+                            "                                                </div>\n" +
+                            "                                                <div class=\"orderNow\">\n" +
+                            "                                                    <div>Заказать сейчас!</div>\n" +
+                            "                                                </div>\n" +
+                            "                                            </div>\n" +
+                            "                                        </div>";
                         $(catKeeper).html(itemsMasonry);
                     }
                 }
 
-            })
+            });
+
+
+
         });
 
 });
